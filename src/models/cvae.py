d0bec6127625ebc2ff91478dcb1da8e6b721d790
Bea Maia <beamaia.work@gmail.com>
2024-02-05 00:54:05 -0300

    WIP: understanding gm-vae's implementation
    In order to understand better the implementation of https://github.com/ml-postech/GM-VAE/blob/main/vae.py , I'm trying to first understand pytorch layers and how they interact with each other. I was able to implement an encoder (diff from gm-vae) that outputs two values (mean and sigma?). Decoder is WIP

--- src/models/cvae.py
+++ src/models/cvae.py
@@ -2,52 +2,35 @@ from torch import nn
 from torch.autograd import Variable
 
 
+class Encoder(nn.Module):
+    def __init__(self):
+        super(Encoder, self).__init__()
+        self.encoder = nn.Sequential(
+                nn.Conv2d(1, 32, 4, 2, 1),
+                nn.LeakyReLU(0.2, inplace=True),  # (32)
+                nn.Conv2d(32, 64, 4, 2, 1),
+                nn.LeakyReLU(0.2, inplace=True),  # (16)
+                nn.Conv2d(64, 128, 4, 2, 1),
+                nn.LeakyReLU(0.2, inplace=True),  # (8)
+                nn.Conv2d(128, 256, 4, 2, 1),
+                nn.LeakyReLU(0.2, inplace=True),  # (4)
+                nn.Flatten(),
+            )
+        self.output_dim = 4 * 4 * 256        
+
+    def forward(self, x):
+        features = self.encoder(x)
+        return features
+
+class Decoder(nn.Module):
+    pass
+
+
 class VAE(nn.Module):
     def __init__(self):
-        super(VAE, self).__init__(image_size_tuple)
-
-        # Encoder
-        self.conv1 = nn.Conv2d(3, 64, kernel_size=3, stride=1)
-        self.bn1 = nn.BatchNorm2d(64)
-        
-        self.conv2 = nn.Conv2d(64, 128, kernel_size=3, stride=2)
-        self.bn2 = nn.BatchNorm2d(128)
-        
-        self.conv3 = nn.Conv2d(128, 256, kernel_size=3, stride=1)
-        self.bn3 = nn.BatchNorm2d(256)
-
-        self.fc1 = nn.Linear(8 * 8 * 16, 512)
-        self.fc_bn1 = nn.BatchNorm1d(512)
-        self.fc21 = nn.Linear(512, 512)
-        self.fc22 = nn.Linear(512, 512)
-
-        # Decoder
-        self.fc3 = nn.Linear(512, 512)
-        self.fc_bn3 = nn.BatchNorm1d(512)
-        self.fc4 = nn.Linear(512, 8 * 8 * 16)
-        self.fc_bn4 = nn.BatchNorm1d(8 * 8 * 16)
-
-        self.conv5 = nn.ConvTranspose2d(16, 32, kernel_size=3, stride=2, padding=1, output_padding=1,)
-        self.bn5 = nn.BatchNorm2d(32)
-        
-        self.conv6 = nn.ConvTranspose2d(32, 32, kernel_size=3, stride=1, padding=1)
-        self.bn6 = nn.BatchNorm2d(32)
-        
-        self.conv7 = nn.ConvTranspose2d(32, 16, kernel_size=3, stride=2, padding=1, output_padding=1)
-        self.bn7 = nn.BatchNorm2d(16)
-        
-        self.conv8 = nn.ConvTranspose2d(16, 3 * 256, kernel_size=3, stride=1, padding=1)
-
-        self.relu = nn.ReLU()
-        
-    def encode(self, x):
-        conv1 = self.relu(self.bn1(self.conv1(x)))
-        conv2 = self.relu(self.bn2(self.conv2(conv1)))
-        conv3 = self.relu(self.bn3(self.conv3(conv2)))
-        conv4 = self.relu(self.bn4(self.conv4(conv3))).view(-1, 8 * 8 * 16)
-
-        fc1 = self.relu(self.fc_bn1(self.fc1(conv4)))
-        return self.fc21(fc1), self.fc22(fc1)
+        super(VAE, self).__init__(self)
+        self.encoder = Encoder()
+        self.decoder = Decoder()
 
     def reparameterize(self, mu, logvar):
         if self.training:
@@ -57,14 +40,14 @@ class VAE(nn.Module):
         else:
             return mu
 
-    def decode(self, z):
-        fc3 = self.relu(self.fc_bn3(self.fc3(z)))
-        fc4 = self.relu(self.fc_bn4(self.fc4(fc3))).view(-1, 16, 8, 8)
 
-        conv5 = self.relu(self.bn5(self.conv5(fc4)))
-        conv6 = self.relu(self.bn6(self.conv6(conv5)))
-        conv7 = self.relu(self.bn7(self.conv7(conv6)))
-        return self.conv8(conv7).view(-1, 256, 3, 32, 32)
+    def encode(self, x):
+        pass
+
+
+    def decode(self, z):
+        pass
+
 
     def forward(self, x):
         mu, logvar = self.encode(x)
